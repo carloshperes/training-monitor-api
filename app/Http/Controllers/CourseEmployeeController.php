f66f9f78398ec055c7a4472c76d45b059f570697
Carlos Peres <carloshperes@gmail.com>
2024-08-05 14:44:25 -0300

    Removendo controllers não utilizadas

--- app/Http/Controllers/CourseEmployeeController.php
+++ app/Http/Controllers/CourseEmployeeController.php
@@ -16,36 +16,4 @@ class CourseEmployeeController extends Controller
             'data'  => CourseEmployee::with('courses', 'employees')->paginate(10)
         ];
     }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
 }
